a243430f521fc0bec4cb6d94279603487dca5434
Daniel Bovington <d.bovington@gmail.com>
2019-04-24 20:31:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,21 @@
 class Dog
   @@all = []
   
-
+=begin
+def initialize(dog)
+  @@all = []
+ end
+  
+  def clear_all(dog)
+    @@all.clear
+  end
+  
+  def all
+    puts @@all
+  end
+  
+  def new(dog)
+    dog << @@all
+  end
+=end
 end
